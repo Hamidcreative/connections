1677c8b1448f3e3cc33029ff1cc243d2d4751a82
hamidcreative <hamid.creativetech@gmail.com>
2019-10-22 18:41:06 +0500

    initial commit

--- connections/settings.py
+++ connections/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os.path
 
-import PyMySQL
-PyMySQL.install_as_MySQLdb()
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
