42b40a9c835748826668bd0a8508a0d105cf39e8
hamidcreative <hamid.creativetech@gmail.com>
2019-10-24 01:09:06 +0500

    initial commit

--- connections/settings.py
+++ connections/settings.py
@@ -160,7 +160,7 @@ AUTH_USER_MODEL = 'linkedin.User'
 # STATICFILES_DIRS = ( os.path.join('static'), )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = ()
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statics'),
