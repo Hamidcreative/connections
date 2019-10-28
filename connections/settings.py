71cdbe0ed0749f9e4f353809a4b00e1a957d8798
hamidcreative <hamid.creativetech@gmail.com>
2019-10-28 19:17:56 +0500

    initial commit

--- connections/settings.py
+++ connections/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.psycopg2',
         'NAME': 'connections',
         'USER': 'postgres',
         #'PASSWORD': 'postgres',
