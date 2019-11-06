9fb2da12e4d73741204376552cc1925b06c0f924
hamidcreative <hamid.creativetech@gmail.com>
2019-11-06 12:20:11 +0500

    initial commit

---
+++ connections/middleware/authenticatemiddleware.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponseRedirect,  HttpResponse
+import requests
+class AuthenticateMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    def get_user_id(self, request, exception):
+
+        return  request.user.id
+
