e48f95d89515e375ac66d4b64b834a708deed8de
hamidcreative <hamid.creativetech@gmail.com>
2019-11-07 19:36:20 +0500

    initial commit

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.conf import settings
 
 
-class Post(models.Model):
+class Postssss(models.Model):
     author     = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title      = models.CharField(max_length=100) # Name
     status     = models.IntegerField(default=0)
